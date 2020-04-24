df5d23e68791b82fa7bdde3cf1c133a8a559a8a4
danilosimoes <danilossimoes28@gmail.com>
2020-04-23 22:44:43 -0300

    nova estrutura

---
+++ python basico/estrutura-de-dados.py
@@ -0,0 +1,7 @@
+minha_lista = ['Dan', 'Gui']    #Lista (list
+minha_tupla = ('Dan', 'Gui')    #Tupla (tuple)
+meu_dicionario = {'nome': 'Danilo', 'Idade': 26}   # (dict)
+meu_conjunto = {'Dan', 'Gui'}     #(set)
+
+loucura = [(1, 2), (3,4), ({'Joao','maria'},{'Diego'})]
+print(loucura)
