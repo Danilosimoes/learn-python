fa50145d4cd2fe4f344ed8afc2f8ecd72b77c89f
danilosimoes <danilossimoes28@gmail.com>
2020-06-19 15:01:49 -0300

    learning about package

---
+++ udemy-course/Package/pacotes/pacotev3.py
@@ -0,0 +1,6 @@
+from pacote1 import modulo1
+from pacote2 import modulo1 as modulo1_sub
+# não se confundir com nomes nos pacotes
+
+print('Soma', modulo1.soma(3, 2))
+print('Subtração', modulo1_sub.sub(3, 2))
---
+++ udemy-course/Package/pacotes/pacotev4.py
@@ -0,0 +1,5 @@
+from pacote1.modulo1 import soma
+from pacote2.modulo1 import sub as subtracao
+
+print("Soma", soma(3, 2))
+print('Subtração', subtracao(3, 2))
