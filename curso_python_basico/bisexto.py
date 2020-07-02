d33d0781b866db92563946f001ebda5ed50e2002
danilosimoes <danilossimoes28@gmail.com>
2020-07-02 17:37:11 -0300

    studing poo 2

--- curso_python_basico/bisexto.py
+++ curso_python_basico/bisexto.py
@@ -1,9 +1,10 @@
 from datetime import date
+
 ano = int(input('Que ano quero analisar? Coloque 0 para analisar o ano atual: '))
 if ano == 0:
     ano = date.today().year
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('O ano de {} é Bissexto'.format(ano))
 else:
-    #print('O ano de {} Não é Bissexto'.format(ano))
+    # print('O ano de {} Não é Bissexto'.format(ano))
     print(f'O ano de {ano} Não é Bissexto')
---
+++ udemy-course/poo2/contador_objetos.py
@@ -0,0 +1,15 @@
+class ClassesSimples:
+    contador = 0
+
+    def __init__(self):
+        # self.__class__.contador += 1
+        self.contar()
+
+    @classmethod
+    def contar(cls):
+        cls.contador += 1
+
+
+if __name__ == '__main__':
+    lista = [ClassesSimples(), ClassesSimples()]
+    print(ClassesSimples.contador)
