95afc5ef11d1181c3f3852266aaa567ed5a68844
danilosimoes <danilossimoes28@gmail.com>
2020-07-04 13:35:40 -0300

    new code

--- curso_python_basico/Operadores-logicos.py
+++ curso_python_basico/Operadores-logicos.py
@@ -1,6 +1,6 @@
 import math
 
-
+'''
 idade = int(input('Qual a sua Idade?: '))
 altura = float(input('Qual a sua Altura?: '))
 peso = float(input('Qual o seu peso?: '))
@@ -9,3 +9,24 @@ if idade >= 18 and altura >= 1.70 and peso >= 70:
     print('Você está apto para entrar no Exército!')
 else:
     print('Você não está apto para entrar no Exército!')
+    
+'''
+
+
+def join_army(age, height, weight):
+    age = float(age)
+    height = float(height)
+    weight = float(weight)
+
+    if age >= 18 and height >= 1.70 and weight >= 70:
+        return "You can to join the army"
+    else:
+        return "You can't to join the army"
+
+
+if __name__ == '__main__':
+    age = input('Type your age: ')
+    height = input('Type your height: ')
+    weight = input('Type your weight: ')
+    data = join_army(age, height, weight)
+    print(data)
