d371443e245138ac1150520bc30fb35b37bd1142
danilossimoes <danilossimoes28@gmail.com>
2021-09-29 20:40:55 -0300

    updating

---
+++ curso_python_basico/aow.py
@@ -0,0 +1,14 @@
+nome = int(input("Digite o número"));
+
+nota = "200"
+nota2 = "100"
+
+if nota >= nome <= 300:
+    print("Voce passou no curso")
+elif nome > nota2:
+    print("bora que bora")
+    if nota2 >700:
+        print("passamos")
+else:
+    print("moio")
+
